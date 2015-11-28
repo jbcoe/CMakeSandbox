b2d787205eeac3f74a01479e06e07d2591e4f8aa
Jonathan Coe <jon@jbcoe.net>
2015-11-28 22:41:42 +0000

    tidyup commented out code

--- StaticInitialization_ODR/Singleton.h
+++ StaticInitialization_ODR/Singleton.h
@@ -1,8 +1,8 @@
 #include <array>
 #include <vector>
 
-//std::vector<int> 
 std::array<int,6> g_magic_numbers = {4,7,22,1,7,11};
+//std::vector<int> g_magic_numbers = {4,7,22,1,7,11};
 
 size_t g_i = 0;
 
