97620bb71f17208ad533126329ef34e5940c3812
Jonathan Coe <jbcoe@me.com>
2015-11-28 14:16:08 +0000

    Run with exit 0 but ODR issues

--- StaticInitialization_ODR/Singleton.h
+++ StaticInitialization_ODR/Singleton.h
@@ -1,12 +1,12 @@
+#include <array>
 #include <vector>
-#include <random>
 
-std::vector<int> g_magic_numbers = {4,7,22,1,7,11};
+//std::vector<int> 
+std::array<int,6> g_magic_numbers = {4,7,22,1,7,11};
 
 size_t g_i = 0;
 
 int magic_number()
 {
-  std::uniform_int_distribution<int> d(0,g_magic_numbers.size()-1);
   return g_magic_numbers[(g_i++)%g_magic_numbers.size()];
 }
