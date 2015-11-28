917da099c2076022aaab4173a191c745d17cb764
Jonathan Coe <jbcoe@me.com>
2015-11-28 13:12:21 +0000

    reproducible ODR-induced crash

--- StaticInitialization_ODR/main.cpp
+++ StaticInitialization_ODR/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 int favourite_number();
+int least_favourite_number();
 
 int main()
 {
   std::cout << "My favourite number is " << favourite_number() << '\n';
+  std::cout << "My least favourite number is " << least_favourite_number() << '\n';
 }
